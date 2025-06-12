73e6750fd615602764932b117b2c04a6980457ed
Suman Giri <sumanrazgiri109@gmail.com>
2025-06-12 15:10:44 +0545

    just testing

--- lib/main.dart
+++ lib/main.dart
@@ -14,7 +14,8 @@ class MyApp extends StatelessWidget {
       title: 'Flutter Demo',
       theme: ThemeData(
         // This is the theme of your application.
-        
+        //this is added by suman
+
         //
         // TRY THIS: Try running your application with "flutter run". You'll see
         // the application has a purple toolbar. Then, without quitting the app,
